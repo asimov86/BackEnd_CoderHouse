8a1bdc79001175c596dfe862e5a0aeaab3e34c67
asimov86 <kennyjosue8@gmail.com>
2023-01-29 23:04:05 -0300

    Revisión

--- Desafio_WS_HDBS/src/app.js
+++ Desafio_WS_HDBS/src/app.js
@@ -1,19 +1,19 @@
 const express = require('express')
 const path = require('path')
-const publicPath = path.resolve(__dirname, './public');
-const handlebars = require('express-handlebars');
+// const publicPath = path.resolve(__dirname, './public');
+const { engine } = require('express-handlebars');
 const http = require('http');
 const io = require('socket.io');
 
 const products_api = require('./api/products_api.js');
 
 
-const hbs = handlebars.create({
-    extname: 'hbs',
-    defaultLayout: 'index.hbs',
-    layoutsDir: __dirname + "/views/layaout",
-    partialsDir: __dirname + "/views/partials"
-});
+// const hbs = handlebars.create({
+//     extname: 'hbs',
+//     defaultLayout: 'index.hbs',
+//     layoutsDir: __dirname + "/views/layaout",
+//     partialsDir: __dirname + "/views/partials"
+// });
 
 //--------------------------------------------
 // instancio servidor, socket y api
@@ -33,7 +33,7 @@ myWSServer.on('connection', async socket => {
 
     // carga inicial de productos
     socket.emit('products', productosApi.showAll());
-    console.log(productosApi.showAll());
+    // console.log(productosApi.showAll());
 
     // actualizacion de productos
     socket.on('update', producto => {
@@ -45,12 +45,12 @@ myWSServer.on('connection', async socket => {
 
 
 // Se agrega lo sig para poder trabajar correctamente con lo que nos envian en el body de un POST o PUT
-app.engine('hbs', hbs.engine);
-app.set('view engine', 'hbs');
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
 app.set('views', './views');
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use(express.static(publicPath));
+app.use(express.static('public'));
 
 
 let productos = [
@@ -72,9 +72,16 @@ let productos = [
 
 //GET
 
-app.get("/productos", (req, res) => {
+app.get("/realTimeProducts", (req, res) => {
     const product = productos;
     res.render('realTimeProducts', { 
+        product: product,
+        listExists: true,});
+});
+
+app.get("/home", (req, res) => {
+    const product = productos;
+    res.render('home', { 
         product: product,
         listExists: true,});
 });
