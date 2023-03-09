cae6ab3e10743a0b489f9a08fe0b903a1646a10d
asimov86 <kennyjosue8@gmail.com>
2023-03-08 23:40:09 -0300

    Correcciones

--- SistemaLoginMejorado/src/routes/sessions.router.js
+++ SistemaLoginMejorado/src/routes/sessions.router.js
@@ -32,10 +32,10 @@ router.post('/register', async (req, res) => {
 
 router.get('/github', passport.authenticate('github', {scope: ['user:email']}, async(req, res)=>{}));
 
-router.get('/githubcallback', passport.authenticate('github', {failureRedirect: '/login'}, async(req, res)=>{
+router.get('/githubcallback', passport.authenticate('github', {failureRedirect: '/login'}), async(req, res)=>{
     req.session.user = req.user;
-    res.redirect('/');
-}));
+    res.redirect('/products');
+});
 
 
 /* router.post('/login', async (req, res) => {
